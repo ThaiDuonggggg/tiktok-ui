b111add79da7306fcf70a819902b69a9fdb385ad
ThaiDuonggggg <hongdyong990@gmail.com>
2024-04-14 22:17:29 +0700

    6. Xây Dựng UI Phần Header #3. Tạo component Button và truyền props

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,8 +1,9 @@
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { faCircleXmark, faMagnifyingGlass, faPlus, faSpinner } from '@fortawesome/free-solid-svg-icons';
 
+import Button from '~/components/Button';
 import { Wrapper as PopperWrapper } from '~/components/Popper';
 import images from '~/assets/images';
 import Tippy from '@tippyjs/react';
@@ -57,7 +58,10 @@ function Header() {
                 </Tippy>
 
                 <div className={cx('actions')}>
-                    <h1>Actions</h1>
+                    <Button text leftIcon={<FontAwesomeIcon icon={faPlus} />}>
+                        Upload
+                    </Button>
+                    <Button outline>Log in</Button>
                 </div>
             </div>
         </header>
