ef2dc8bdee21f8d9ac21f8ea92ebcc16ee6e0d1c
ThaiDuonggggg <hongdyong990@gmail.com>
2024-04-15 15:53:51 +0700

    8. Xây Dựng UI Phần Header #5. Sử dụng useState() Xử lý logic phần render component Menu.

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -26,6 +26,21 @@ const MENU_ITEMS = [
     {
         icon: <FontAwesomeIcon icon={faEarthAsia} />,
         title: 'English',
+        children: {
+            title: 'Language',
+            data: [
+                {
+                    type: 'language',
+                    code: 'en',
+                    title: 'English',
+                },
+                {
+                    type: 'language',
+                    code: 'vi',
+                    title: 'Tiếng Việt',
+                },
+            ],
+        },
     },
     {
         icon: <FontAwesomeIcon icon={faCircleQuestion} />,
@@ -46,6 +61,17 @@ function Header() {
             setSearchResult([]);
         }, 0);
     }, []);
+
+    const handleMenuOnchange = (menuItem) => {
+        switch (menuItem) {
+            case 'language':
+                // Handle change Language
+                break;
+
+            default:
+                break;
+        }
+    };
 
     return (
         <header className={cx('wrapper')}>
@@ -89,7 +115,7 @@ function Header() {
                     </Button>
                     <Button primary>Log in</Button>
 
-                    <Menu items={MENU_ITEMS}>
+                    <Menu items={MENU_ITEMS} onChange={handleMenuOnchange}>
                         <button className={cx('more-btn')}>
                             <FontAwesomeIcon icon={faEllipsisVertical} />
                         </button>
